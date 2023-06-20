6f08f3bc53c49c3c9180653ce7f53fc0287da1f1
Moonyoung Chu <cnansdud11@hanmail.net>
2023-06-20 15:23:49 +0900

    feat: 사용자 입력 유효성검사 추가
    - 125, 126강

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (
+        validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (
+        validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid =
+            isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (
+        validatableInput.min != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (
+        validatableInput.max != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+
+
 // autobind decorator
 function autobind(
     _: any,
@@ -25,7 +69,6 @@ class ProjectInput {
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         // document.importNode() 함수는 DOM에서 노드를 가져와 새로운 노드의 복제본을 생성하는 메서드입니다. 
@@ -35,7 +78,6 @@ class ProjectInput {
 
         // 가져온 내용의 첫 번째 자식 요소를 가져와 HTMLFormElement로 캐스팅합니다.
         this.element = importedNode.firstElementChild as HTMLFormElement;
-
         this.element.id = 'user-input';
 
         this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
@@ -43,20 +85,63 @@ class ProjectInput {
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
         this.configure();
-
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        };
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+
     @autobind
     private submitHandler(event: Event) {
-      event.preventDefault();
-      console.log(this.titleInputElement.value);
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     }
 
     private configure() {
         this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
-
 
     private attach() {
         // 메서드는 요소의 특정 위치에 다른 요소를 삽입하는 DOM 메서드입니다. 
